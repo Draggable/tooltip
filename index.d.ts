676a14db5aa2caf7374310fdeafe5862152b62fd
kevinchappell <kevinchappell@users.noreply.github.com>
2024-11-13 23:26:15 +0000

    deploy: cf5eab810cbd63c64a3d1a021bccf1e246e5aa21

--- index.d.ts
+++ index.d.ts
@@ -2,19 +2,27 @@ interface SmartTooltipOptions {
     triggerName: string;
 }
 export declare class SmartTooltip {
-    private readonly options;
+    readonly triggerName: string;
     private readonly tooltip;
     private activeTriggerType;
+    private readonly spacing;
     constructor(options?: SmartTooltipOptions);
     private setupEventListeners;
+    private readonly handleClick;
+    private readonly handleMouseOver;
+    private readonly handleMouseOut;
+    private readonly handleResize;
+    private readonly handleScroll;
     private isVisible;
-    calculatePosition(trigger: Element): {
-        name: string;
-        x: number;
-        y: number;
-    };
-    fitsInViewport(x: number, y: number, width: number, height: number): boolean;
+    private calculatePosition;
+    private fitsInViewport;
     private show;
     private hide;
+    destroy(): void;
+}
+declare global {
+    interface Window {
+        SmartTooltip: typeof SmartTooltip;
+    }
 }
 export default SmartTooltip;
